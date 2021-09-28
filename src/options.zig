6731ea677a5e4e2d7060233c5a3458c2063b2caa
Sören Tempel <soeren+git@soeren-tempel.net>
2021-09-28 15:04:27 +0200

    Add some option constants

--- src/options.zig
+++ src/options.zig
@@ -2,3 +2,8 @@ pub const Option = struct {
     number: u32,
     value: []const u8,
 };
+
+// https://datatracker.ietf.org/doc/html/rfc7252#section-5.10
+pub const IfMatch: u32 = 1;
+pub const URIHost: u32 = 3;
+pub const URIPath: u32 = 11;
