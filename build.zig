1bad09dced94097f7851c0bd71d51a93c4f703c4
Sören Tempel <soeren+git@soeren-tempel.net>
2021-09-28 08:59:32 +0200

    main.zig → zoap.zig

--- build.zig
+++ build.zig
@@ -2,13 +2,13 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("zoap", "src/main.zig");
+    const lib = b.addStaticLibrary("zoap", "src/zoap.zig");
     lib.setBuildMode(mode);
     lib.install();
 
-    var main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
+    var zoap_tests = b.addTest("src/zoap.zig");
+    zoap_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&zoap_tests.step);
 }
