5b32b51beb144f953cbcc6bfebe11b13b274fd99
Sören Tempel <soeren+git@soeren-tempel.net>
2021-10-21 17:11:40 +0200

    Simplify WriteBuffer implementation

--- src/buffer.zig
+++ src/buffer.zig
@@ -4,36 +4,28 @@ pub const WriteBuffer = struct {
     slice: []u8,
     pos: usize = 0,
 
-    pub fn byte(self: *WriteBuffer, b: u8) !void {
-        if (self.slice.len - self.pos < @sizeOf(u8))
-            return error.OutOfBounds;
-
-        self.slice[self.pos] = b;
-        self.pos += @sizeOf(u8);
-    }
-
-    pub fn half(self: *WriteBuffer, h: u16) !void {
-        if (self.slice.len - self.pos < @sizeOf(u16))
-            return error.OutOfBounds;
-
-        mem.copy(u8, self.slice[self.pos..], mem.asBytes(&h));
-        self.pos += @sizeOf(u16);
-    }
-
-    pub fn word(self: *WriteBuffer, w: u32) !void {
-        if (self.slice.len - self.pos < @sizeOf(u32))
-            return error.OutOfBounds;
-
-        mem.copy(u8, self.slice[self.pos..], mem.asBytes(&w));
-        self.pos += @sizeOf(u32);
-    }
-
     pub fn bytes(self: *WriteBuffer, buf: []const u8) !void {
         if (self.slice.len - self.pos < buf.len)
             return error.OutOfBounds;
 
         mem.copy(u8, self.slice[self.pos..], buf);
         self.pos += buf.len;
+    }
+
+    fn write(self: *WriteBuffer, ptr: anytype) !void {
+        try self.bytes(mem.asBytes(ptr));
+    }
+
+    pub fn byte(self: *WriteBuffer, b: u8) !void {
+        try self.write(&b);
+    }
+
+    pub fn half(self: *WriteBuffer, h: u16) !void {
+        try self.write(&h);
+    }
+
+    pub fn word(self: *WriteBuffer, w: u32) !void {
+        try self.write(&w);
     }
 };
 
