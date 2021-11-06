572dbf4cdee92e4a1cffac255664030d088632c6
Sören Tempel <soeren+git@soeren-tempel.net>
2021-11-06 19:32:41 +0100

    Unclutter namespace

--- src/zoap.zig
+++ src/zoap.zig
@@ -1,5 +1,13 @@
-pub const pkt = @import("packet.zig");
-pub const res = @import("resource.zig");
+const pkt = @import("packet.zig");
+pub const Msg = pkt.Msg;
+pub const Header = pkt.Header;
+pub const Response = pkt.Response;
+pub const Request = pkt.Request;
+
+const res = @import("resource.zig");
+pub const ResourceHandler = res.ResourceHandler;
+pub const Resource = res.Resource;
+pub const Dispatcher = res.Dispatcher;
 
 pub const codes = @import("codes.zig");
 pub const options = @import("options.zig");
